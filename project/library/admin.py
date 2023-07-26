6d3ae40f7f6ec46f906e8abefa2948bfad469e55
Slb59 <sylvie.bricout2@laposte.net>
2023-07-26 09:42:15 +0200

    add admin for books

--- project/library/admin.py
+++ project/library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+
+@admin.register(Book)
+class ProjectAdmin(admin.ModelAdmin):
+    """ Spécifications for administration of projects """
+    list_display = [
+        "title", "author",
+        "created_time", "updated_time"
+        ]
+    list_filter = ["created_time", "updated_time"]
+    search_fields = ["title"]
+    ordering = ["-created_time"]
